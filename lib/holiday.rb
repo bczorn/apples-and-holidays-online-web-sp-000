02d20febbe518ea8b7786ab3e3158faf4a1cbe80
Becky Zorn <bczorn@gmail.com>
2019-09-11 18:26:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash.each do |season, data|
     if season == :winter
+      binding.pry
       data.push(supply)
       
     end

29e5e6ad1e0f1150554563adac57ef19b5b0e048
Becky Zorn <bczorn@gmail.com>
2019-09-11 19:20:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,7 +106,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+x = []
+  holiday_hash.each do |season, data|
+    data.each do |holiday, data|
+    if data.includes?("BBQ")
+      x << holiday
+    end
+  end
 end
 
 

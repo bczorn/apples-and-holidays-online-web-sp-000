e52d92a7f05337e12c9cf164055f6a8e90c2c809
Becky Zorn <bczorn@gmail.com>
2019-09-11 18:57:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,8 +83,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, x|
+      holiday.split.capitalize!
+      holiday.join(" ")
       l = x.join(", ")
-      puts "  #{holiday.capitalize}: #{l}"
+      puts "  #{holiday}: #{l}"
     end
   end
 

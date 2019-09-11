5a8154d4caebaf2220cad6c1318bb5b8d7cc46da
Becky Zorn <bczorn@gmail.com>
2019-09-11 18:49:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, data|
-    puts "#{season}:"
+    puts "#{season.capitalize}:"
     data.each do |holiday, x|
       puts "#{holiday}: #{x}"
     end
